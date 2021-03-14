1d3bda674978f4e88fa67659985cb3a6c8137435
Brendan Doyle <brendandoyle060@gmail.com>
2021-03-14 11:47:38 -0400

    Fixed issue where button would sometimes not work without refreshing the page first

--- background.js
+++ background.js
@@ -1,22 +1,53 @@
 // background.js
 
 // Called when the user clicks on the browser action.
-chrome.browserAction.onClicked.addListener(function(tab) {
+chrome.browserAction.onClicked.addListener(function inject(tab) {
 
-  // alert("YO " + tab.url);
+    // Break if we're not on a stocks page
+    if (!tab.url || !tab.url.startsWith('https://robinhood.com/stocks/')){
+        return;
+    }
 
-  // Send a message to the active tab
-  chrome.tabs.query({active: true, lastFocusedWindow: true}, function(tabs) {
-    var activeTab = tabs[0];
-    chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action", "url": tab.url});
-  });
+    console.log("created inject listener");
+
+    chrome.tabs.executeScript(null,{file:"content.js"}, function(results){console.log(results);});
+    console.log("chrome.tabs.executeScript content.js");
+
+    // Send a message to the active tab
+    chrome.tabs.query({
+        active: true, 
+        lastFocusedWindow: true
+    }, 
+    function(tabs) {
+        console.log("chrome.tabs.query");
+        console.log("tabs.length: " + tabs.length);
+
+        var activeTab = tabs[0];
+
+        chrome.tabs.sendMessage(activeTab.id, {
+            "message": "clicked_browser_action", 
+            "url": tab.url
+        });
+
+            console.log("sendMessage - tab url: " + tab.url);
+            console.log("end chrome.tabs.query");
+    });
+
 });
 
 // Open new tab
 chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if( request.message === "open_new_tab" ) {
-      chrome.tabs.create({"url": request.url});
+    function openNewTab(request, sender, sendResponse) {
+        console.log("chrome.runtime.onMessage.addListener - open_new_tab");
+        if( request.message === "open_new_tab" ) {
+            chrome.tabs.create({
+                "url": request.url
+            });
+            console.log("opened new tab");
+        }
+
+        console.log(request);
+        console.log(sender);
+
     }
-  }
 );
--- content.js
+++ content.js
@@ -1,20 +1,39 @@
 // content.js
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if( request.message === "clicked_browser_action" ) {
 
-      var ticker = getTicker();
-      console.log(ticker);
-      var yahooBoi = "https://finance.yahoo.com/quote/" + ticker;
-      chrome.runtime.sendMessage({"message": "open_new_tab", "url": yahooBoi});
-      
+(function() {
+    // Ensure that this content script will not run more than once per click
+    if ( window.hasRun ) {
+        return;
     }
-  }
-);
+    window.hasRun = true;
 
-function getTicker() {
-	var titleElement = document.querySelector("title");
-	var title = titleElement.textContent;
-	var splitTitle = title.split(" ");
-	return splitTitle[0];
-}
+    chrome.runtime.onMessage.addListener(
+        function iconClicked(request, sender, sendResponse) {
+
+            if ( request.message === "clicked_browser_action" ) {
+
+              console.log("message received: clicked_browser_action");
+              console.log(request);
+              console.log(sender);
+
+              var ticker = getTicker();
+              console.log(ticker);
+              var yahooBoi = "https://finance.yahoo.com/quote/" + ticker;
+
+              chrome.runtime.sendMessage({
+                "message": "open_new_tab", 
+                "url": yahooBoi
+              });
+
+            }
+        }
+    );
+
+    function getTicker() {
+        var titleElement = document.querySelector("title");
+        var title = titleElement.textContent;
+        var splitTitle = title.split(" ");
+        return splitTitle[0];
+    }
+
+})();
