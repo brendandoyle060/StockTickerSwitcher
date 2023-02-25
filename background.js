1ee88a6d543fac4dc75c849c09a44bf472dc94e8
Brendan Doyle <brendandoyle060@gmail.com>
2023-02-25 18:16:37 -0500

    Manifest v2 -> v3

--- background.js
+++ background.js
@@ -1,12 +1,12 @@
 // background.js
 
 // Called when the user clicks on the browser action.
-chrome.browserAction.onClicked.addListener(function inject(tab) {
+chrome.action.onClicked.addListener(function inject(tab) {
 
-    // console.log("browserAction onClicked");
+    // console.log("action onClicked");
 
     // Break if we're not on a stocks or crypto page
-    if ( !isValidPage(tab.url) ){
+    if (!isValidPage(tab.url)) {
         // console.log("if !isValidPage");
         return;
     }
@@ -14,39 +14,39 @@ chrome.browserAction.onClicked.addListener(function inject(tab) {
     // console.log("created inject listener");
 
     // var myCode = "console.log('Testing...this code will execute as a content script');";
-    chrome.tabs.executeScript(
-        null,
+    chrome.scripting.executeScript(
         {
-            file: "content.js"
-        }, 
-        function(){
-            if(chrome.runtime.lastError) {
+            target: { tabId: tab.id },
+            files: ["content.js"]
+        },
+        function () {
+            if (chrome.runtime.lastError) {
                 console.error("Script injection failed: " + chrome.runtime.lastError.message);
             }
         }
     );
-    // console.log("chrome.tabs.executeScript content.js");
+    // console.log("chrome.scripting.executeScript content.js");
 
     // Send a message to the active tab
     chrome.tabs.query({
-        active: true, 
+        active: true,
         lastFocusedWindow: true
-    }, 
-    function(tabs) {
-        // console.log("chrome.tabs.query");
-        // console.log("tabs.length: " + tabs.length);
+    },
+        function (tabs) {
+            // console.log("chrome.tabs.query");
+            // console.log("tabs.length: " + tabs.length);
 
-        var activeTab = tabs[0];
-        var index = activeTab.index;
-        chrome.tabs.sendMessage(activeTab.id, {
-            "message": "clicked_browser_action", 
-            "url": tab.url,
-            "index": index + 1
-        });
+            var activeTab = tabs[0];
+            var index = activeTab.index;
+            chrome.tabs.sendMessage(activeTab.id, {
+                "message": "clicked_browser_action",
+                "url": tab.url,
+                "index": index + 1
+            });
 
             // console.log("sendMessage - tab url: " + tab.url);
             // console.log("end chrome.tabs.query");
-    });
+        });
 
 });
 
@@ -64,7 +64,7 @@ function isValidPage(url) {
 chrome.runtime.onMessage.addListener(
     function openNewTab(request, sender, sendResponse) {
         // console.log("chrome.runtime.onMessage.addListener - open_new_tab");
-        if( request.message === "open_new_tab" ) {
+        if (request.message === "open_new_tab") {
             chrome.tabs.create({
                 url: request.url,
                 index: request.index
