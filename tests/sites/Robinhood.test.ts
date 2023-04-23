a6a89ece1a7d5d9ebd8752ef8725b8c1548ce543
Brendan Doyle <brendandoyle060@gmail.com>
2023-04-23 18:31:07 -0400

    Added first tests for Robinhood.createUrllForTicker

--- tests/sites/Robinhood.test.ts
+++ tests/sites/Robinhood.test.ts
@@ -1,4 +1,7 @@
 import { Robinhood } from "../../src/sites/Robinhood";
+import { Crypto } from "../../src/tickers/Crypto";
+import { Etf } from "../../src/tickers/Etf";
+import { Stock } from "../../src/tickers/Stock";
 import { Ticker } from "../../src/tickers/Ticker";
 import { JSDOM } from "jsdom";
 
@@ -7,7 +10,7 @@ import { getDoc } from "../utils/helpers";
 
 describe("Testing Robinhood", () => {
     var rh: Robinhood = new Robinhood();
-
+    // Tests for RH -> YF
     test.concurrent.each([
         [
             "stock-gme",
@@ -48,6 +51,21 @@ describe("Testing Robinhood", () => {
 
             expect(rh.isEtf(doc)).toEqual(isEtf);
             expect(rh.isCrypto(url)).toEqual(isCrypto);
+        }
+    );
+
+    // Tests for YF -> RH
+    test.concurrent.each([
+        [new Stock("GME", "GameStop"), "https://robinhood.com/stocks/GME"],
+        [
+            new Etf("SPY", "SPDR S&P 500 ETF"),
+            "https://robinhood.com/stocks/SPY",
+        ],
+        [new Crypto("ETH", "Ethereum"), "https://robinhood.com/crypto/ETH"],
+    ])(
+        "createUrlForTicker on %s, expecting %s",
+        (ticker: Ticker, url: string) => {
+            expect(rh.createUrlForTicker(ticker)).toEqual(url);
         }
     );
 });
