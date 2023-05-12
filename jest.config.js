f8868950b16a5e17038439c62068c5935007dcfd
Brendan Doyle <brendandoyle060@gmail.com>
2023-05-12 15:54:00 -0400

    Adjusted jest.config.js to resolve jest warning

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,14 @@
 module.exports = {
-    transform: { "^.+\\.ts?$": "ts-jest" },
+    transform: {
+        "^.+\\.ts?$": [
+            "ts-jest",
+            {
+                tsconfig: "tsconfig.json",
+            },
+        ],
+    },
     testEnvironment: "node",
     testRegex: "/tests/.*/.*\\.(test|spec)?\\.(ts|tsx)$",
     moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
     setupFiles: ["./tests/utils/helpers.ts"],
-    globals: {
-        "ts-jest": {
-            tsconfig: "tsconfig.json",
-        },
-    },
 };
