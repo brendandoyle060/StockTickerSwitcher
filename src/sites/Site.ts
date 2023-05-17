244ee11a532042454521859fd48ff26344f8b4ff
Brendan Doyle <brendandoyle060@gmail.com>
2023-05-17 19:21:08 -0400

    Added comments for Site.ts

--- src/sites/Site.ts
+++ src/sites/Site.ts
@@ -1,6 +1,10 @@
 import { Ticker } from "../tickers/Ticker";
 import { TickerType } from "../tickers/TickerType";
 
+/**
+ * Parent class for objects holding information
+ * about a particular website.
+ */
 export class Site {
     name: string;
     baseUrl: string;
@@ -11,12 +15,29 @@ export class Site {
         this.baseUrl = baseUrl;
     }
 
+    /**
+     * @returns a string representation of the name of the site
+     */
     is(): string {
         return this.name;
     }
 
+    /**
+     * Given a Ticker, create the appropriate url for that Ticker
+     * on this Site.
+     * @param ticker - the Ticker for which we're creating a url
+     */
     createUrlForTicker(ticker: Ticker): any {}
 
+    /**
+     * Determine the type of ticker for the current page
+     * (stock, crypto, etc)
+     * @param site - the Site which the user is currently on
+     * @param cryptoArg - some sites may need a separate way to identify whether or not the page is a crypto page (such as the url).
+     * If that's the case, that arg goes here. Otherwise, the Document param is usually sent twice.
+     * @param document - the DOM of the current page, as a Document object.
+     * @returns TickerType of the current page
+     */
     getTickerType(site: Site, cryptoArg: any, document: Document): TickerType {
         if (site.isEtf(document)) {
             return new TickerType("etf");
@@ -27,10 +48,28 @@ export class Site {
         }
     }
 
-    // Return type here is "any", because we don't want to return a value in the superclass, which we would have to since we're using them in getTickerType above.
+    // Return type for the methods below is "any", because we don't want to return a value in the superclass, which we would have to since we're using them in getTickerType above.
     // Implementations of these methods in subclasses should return a boolean.
+
+    /**
+     * Returns true if the current page is for a cryptocurrency,
+     * false otherwise.
+     * @param x - arg of any type which will help a partiular site
+     * identify a cryptocurrency page.
+     */
     isCrypto(x: any): any {}
+
+    /**
+     * Returns true if the current page is for an ETF,
+     * false otherwise.
+     * @param document - DOM of the current page.
+     */
     isEtf(document: Document): any {}
 
+    /**
+     * Returns true if the current page is a No Results page,
+     * false otherwise.
+     * @param document - DOM of the current page.
+     */
     isNoResultsPage(document: Document): any {}
 }
